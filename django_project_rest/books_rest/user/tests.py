0d982e1791502500e4cfe46de03b18e9855fb0a6
iYoQ <iyoqpwnz@gmail.com>
2021-11-09 21:21:55 +0300

    hw test

--- django_project_rest/books_rest/user/tests.py
+++ django_project_rest/books_rest/user/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import *
+from rest_framework import status
+from django.test import Client
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+
+class RegistrationTestCase(APITestCase):
+    def test_user_authorization(self):
+        data = {
+            'email': 'admin@admin.com',
+            'password': 'admin',
+        }
+        c = APIClient()
+        # c.login(email='admin@admin.com', password='admin')
+        response = c.post('/user/token/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
